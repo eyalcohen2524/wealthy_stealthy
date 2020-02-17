8ac44b805802e5d3df365601d536f74f76291015
Miles Bader <milesdbader@gmail.com>
2020-02-17 11:48:50 +0200

    index show new create; set_package, package_params

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,22 +1,43 @@
 class PackagesController < ApplicationController
-  def create
-  end
+  before_action :set_package, only: %i[show]
 
-  def destroy
+  def index
+    @packages = Package.all
   end
 
   def show
   end
 
-  def index
+  def new
+    @package = Package.new
   end
 
-  def new
+  def create
+    @package = Package.new(package_params)
+    if @package.save
+      redirect_to packages_path
+    else
+      render :new
+    end
   end
 
   def edit
+
   end
 
   def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_package
+    @package = Package.find(params[:id])
+  end
+
+  def package_params
+    params.require(:package).permit(:price, :description, :content, :name)
   end
 end
