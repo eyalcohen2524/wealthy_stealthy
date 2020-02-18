56ac5fd9a05d7c2c798cc155ff7b8c5f5e5a5f9c
Miles Bader <milesdbader@gmail.com>
2020-02-18 21:57:34 +0200

    removed image_url from packages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_095401) do
+ActiveRecord::Schema.define(version: 2020_02_18_195339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2020_02_18_095401) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_url"
     t.index ["user_id"], name: "index_packages_on_user_id"
   end
 
