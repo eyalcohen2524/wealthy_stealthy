52e3583c5657247eb7f6b4c70dc3c5cdd837a4a9
Miles Bader <milesdbader@gmail.com>
2020-02-18 12:22:52 +0200

    added seeds w users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning the database'
+
+Rental.destroy_all if Rails.env.development?
+Package.destroy_all if Rails.env.development?
+User.destroy_all if Rails.env.development?
+
+puts 'database empty'
+
+
+puts 'Generating 2 users'
+
+miles = User.create!(
+  email: 'milesdbader@gmail.com',
+  password: '123456789',
+  username: 'milesdbader'
+)
+
+eyal = User.create!(
+  email: 'eyalcohen2524@gmail.com',
+  password: '123456789',
+  username: 'eyalcohen2524'
+)
+
+puts 'Two users generated'
+puts "You now have #{User.count} users"
+
 
 puts 'generating 3 packages'
 
-Package.create(
+pack1 = Package.create(
   price: 200,
   description: 'Look stylish in an instant',
-  content: 'Fancy suit; rolex watch; sleek, modern glasses;'
+  content: 'Fancy suit; rolex watch; sleek, modern glasses;',
   name: 'Styled Living'
 )
+pack1.user = miles
+pack1.save!
 
-Package.create(
+pack2 = Package.create(
   price: 500,
   description: 'Make your house look classy before your guests arrive',
   content: 'Tableware set (China) and Silver; 2 paintings; 1 Chandelier;',
   name: 'Classic home'
 )
+pack2.user = eyal
+pack2.save!
 
-Package.create(
+pack3 = Package.create(
   price: 1500,
   description: 'Show your date that you\'re cooler than James Bond, but only for the night',
   content: 'Bond-style suit; Fingerprinted hand gun; Aston Martin (display only, no keys); Cocktail - shaken, not stirred;',
   name: 'James Bond'
 )
+pack3.user = eyal
+pack3.save!
 
 puts 'Three packages generated'
 puts "You now have #{Package.count} packages"
