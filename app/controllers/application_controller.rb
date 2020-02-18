e0bdd86ce610609417898b674c42cd7947fb702a
Miles Bader <milesdbader@gmail.com>
2020-02-18 22:44:56 +0200

    upload picture on registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :photo])
 
   end
 end
